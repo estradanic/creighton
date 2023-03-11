26c6e4d791f143c5c223cf6312da33111ba7053c
Nicholas Estrada <github@nicholasjestrada.aleeas.com>
2023-03-11 08:56:49 -0800

    asdflkasjdflkadsf

--- cloud/app.js
+++ cloud/app.js
@@ -13,11 +13,16 @@ const frontendRoutes = [
 ];
 app.enable("trust proxy");
 app.use((req, res, next) => {
+  req.secure || app.get("X-Forwarded-Proto") !== "http"
+    ? next()
+    : res.redirect("https://" + req.headers.host + req.url);
+});
+app.use((req, res, next) => {
   const pathParts = req.path.split("/").filter((pathPart) => pathPart !== "");
   const serveIndex = req.method === "GET" &&
         (frontendRoutes.includes(pathParts[0]) || pathParts.length === 0);
   if (serveIndex) {
-    res.sendFile(path1.default.join(__dirname, "/public/index.html"));
+    res.sendFile(path1.default.join(__dirname, "public/index.html"));
   } else {
     next();
   }
